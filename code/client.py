05e541e5793a8c54b13c3239c93e3a15a5533ac3
Magicien <dimingueguim19@gmail.com>
2018-11-24 17:12:43 +0000

    premiere version du client serveur valide dans le quel le serveur demande et le client repond

--- code/client.py
+++ code/client.py
@@ -2,4 +2,12 @@ import socket
 
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-connexion_avec_serveur.connect(('localhost', 12800))
+connexion_avec_serveur.connect(('localhost', 12809))
+
+connexion_avec_serveur.send(b"je suis en ligne")
+
+while 1:
+    rec = connexion_avec_serveur.recv(1024)
+    print(rec)
+
+    connexion_avec_serveur.send(b"resultat de la commande")
--- code/serveur.py
+++ code/serveur.py
@@ -1,12 +1,25 @@
 import socket
 
-port = 0
-hote = ''
+port = 12809
+hote = 'localhost'
 
 connexion_prin = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 connexion_prin.bind((hote,port))
-connexion_avec_client, infos_connexion = connexion_principale.accept()
+
+connexion_prin.listen(5)
+connexion_avec_client, infos_connexion = connexion_prin.accept()
+while 1:
 
 
+
+    
+    commande = input("terminal>")
+    connexion_avec_client.send(commande.encode())
+    mess = b""
+    while mess == b"":
+        print (1)
+        mess = connexion_avec_client.recv(1024)
+
+    print(mess)
