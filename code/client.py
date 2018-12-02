4e8939b06739ebb984d4c638374356aaf25db608
Magicien <dimingueguim19@gmail.com>
2018-12-02 01:06:00 +0000

    nous avons integre dans notre code la fonctionalite de transfer des fichiers du repertoire courant par le FTP. ainsi en une commmande nous pouvons telecharger les fichiers

--- code/client.py
+++ code/client.py
@@ -1,13 +1,114 @@
 import socket
+import os
+import time
 
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+etat = 0
+while etat == 0:
+    try:
+        connexion_avec_serveur.connect(('localhost', 12800))
+        etat = 1
+    except:
+        print ("echec")
 
-connexion_avec_serveur.connect(('localhost', 12809))
 
 connexion_avec_serveur.send(b"je suis en ligne")
+
+
+def upload(nomFich):
+    if nomFich != "":
+        try:
+            fich = open(nomFich, "rb")  # test si le fichier existe
+            fich.close()
+
+            octets = os.path.getsize(nomFich) / 1024
+
+            donne = "NAME " + nomFich + "OCTETS " + str(octets)
+            connexion_avec_serveur.send(donne.encode()) # Envoi du nom et de la taille du fichier
+
+            # Boucle temps que l'ont est connecte
+            ############################################
+            while (connexion_avec_serveur.connect):
+
+                recu = connexion_avec_serveur.recv(1024)
+                if not recu: break
+
+                if recu == b"GO":  # Si le serveur accepte on envoi le fichier
+                    print(" >> Le serveur accepte le transfert")
+                    print(time.strftime(" >> [%H:%M] transfert en cours veuillez patienter..."))
+                    print(" ")
+
+                    num = 0
+                    pourcent = 0
+                    octets = octets * 1024  # Reconverti en octets
+                    fich = open(nomFich, "rb")
+                    print("on envoie")
+                    if octets > 1024:  # Si le fichier est plus lourd que 1024 on l'envoi par paquet
+                        for i in range(int(octets / 1024)):
+
+                            fich.seek(num,
+                                      0)  # on se deplace par rapport au numero de caractere (de 1024 a 1024 octets)
+                            donnees = fich.read(1024)  # Lecture du fichier en 1024 octets
+                            connexion_avec_serveur.send(donnees)  # Envoi du fichier par paquet de 1024 octets
+                            num = num + 1024
+
+                            # Condition pour afficher le % du transfert (pas trouve mieu) :
+                            if pourcent == 0 and num > octets / 100 * 10 and num < octets / 100 * 20:
+                                print(" >> 10%")
+                                pourcent = 1
+                            elif pourcent == 1 and num > octets / 100 * 20 and num < octets / 100 * 30:
+                                print(" >> 20%")
+                                pourcent = 2
+                            elif pourcent < 3 and num > octets / 100 * 30 and num < octets / 100 * 40:
+                                print(" >> 30%")
+                                pourcent = 3
+                            elif pourcent < 4 and num > octets / 100 * 40 and num < octets / 100 * 50:
+                                print(" >> 40%")
+                                pourcent = 4
+                            elif pourcent < 5 and num > octets / 100 * 50 and num < octets / 100 * 60:
+                                print(" >> 50%")
+                                pourcent = 5
+                            elif pourcent < 6 and num > octets / 100 * 60 and num < octets / 100 * 70:
+                                print(" >> 60%")
+                                pourcent = 6
+                            elif pourcent < 7 and num > octets / 100 * 70 and num < octets / 100 * 80:
+                                print(" >> 70%")
+                                pourcent = 7
+                            elif pourcent < 8 and num > octets / 100 * 80 and num < octets / 100 * 90:
+                                print(" >> 80%")
+                                pourcent = 8
+                            elif pourcent < 9 and num > octets / 100 * 90 and num < octets / 100 * 100:
+
+                                pourcent = 9
+
+                    else:  # Sinon on envoi tous d'un coup
+                        donnees = fich.read()
+                        connexion_avec_serveur.send(donnees)
+
+                    fich.close()
+                    print("")
+                    print(time.strftime(" >> Le %d/%m a %H:%M transfert termine !"))
+                    connexion_avec_serveur.send(b"BYE")
+
+        except:
+            # le fichier est introuvable
+            connexion_avec_serveur.send("error1".encode())
+            exit()
+
 
 while 1:
     rec = connexion_avec_serveur.recv(1024)
     print(rec)
+    if rec.split()[0] == b"upload":
+        print("on entre dans upload")
+        upload(rec.split()[1].decode())
+    # rep = os.popen(rec.decode())
+    # print(rep)
 
-    connexion_avec_serveur.send(b"resultat de la commande")
+    # connexion_avec_serveur.send(rep.read().encode())
+
+
+
+
+
+
--- code/serveur.py
+++ code/serveur.py
@@ -1,6 +1,9 @@
 import socket
+import os
+import time
 
-port = 12809
+
+port = 12800
 hote = 'localhost'
 
 connexion_prin = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,16 +13,105 @@ connexion_prin.bind((hote,port))
 
 connexion_prin.listen(5)
 connexion_avec_client, infos_connexion = connexion_prin.accept()
+recu = connexion_avec_client.recv(1024)
+print(recu.decode())
+def receive_upload():
+    accepte = "non"
+    #etat = 1
+    while (connexion_avec_client.connect):
+        recu = connexion_avec_client.recv(1024)
+
+        if accepte == "non": # Condition si on a pas deja envoyer le nom et la taille du fichier
+                try:
+                    nomFich = recu.split(b"NAME ")[1]
+                    nomFich = nomFich.split(b"OCTETS ")[0]
+                    nomFich = nomFich.decode()
+                    taille = recu.split(b"OCTETS ")[1]
+                    taille = taille.decode()
+                except:
+                    print(">> ##" + recu.split()[0].decode() + "  fichier introuvable")
+                    return 0
+                print (" >> Fichier '" + nomFich + "' [" + taille + " Ko]")
+
+                accepte = input(" >> Acceptez vous le transfert [o/n] : ") # demande si on accepte ou pas le transfert                               
+
+                if accepte == "o" or accepte == "oui": # Si oui en lenvoi au client et on cree le fichier
+                    connexion_avec_client.send(b"GO")
+                    print (time.strftime(" >> [%H:%M] transfert en cours veuillez patienter..."))
+                    print ("")
+                    f = open(nomFich, "wb")
+                    
+                    taille = float(taille) * 1024 # Conversion de la taille en octets pour le %
+                                       
+                else :
+                    # Si pas accepte on ferme le transfert
+                    print("transfert annule")
+                    return 0
+
+        
+        elif recu == b"BYE": # Si on a recu "BYE" le transfer est termine
+            f.close()
+            print ("")
+            accepte == "non"
+            print (time.strftime(" >> Le %d/%m a %H:%M transfert termine !"))
+            etat = 1
+            return 2
+
+        else: # Sinon on ecrit au fur et a mesure dans le fichier
+            f.write(recu)
+            if taille > 1024: # Si la taille est plus grande que 1024 on s'occupe du %
+
+                # Condition pour afficher le % du transfert :
+                if pourcent == 0 and num > taille / 100 * 10 and num < taille / 100 * 20:
+                    print (" >> 10%")
+                    pourcent = 1
+                elif pourcent == 1 and num > taille / 100 * 20 and num < taille / 100 * 30:
+                    print (" >> 20%")
+                    pourcent = 2
+                elif pourcent < 3 and num > taille / 100 * 30 and num < taille / 100 * 40:
+                    print (" >> 30%")
+                    pourcent = 3
+                elif pourcent < 4 and num > taille / 100 * 40 and num < taille / 100 * 50:
+                    print (" >> 40%")
+                    pourcent = 4
+                elif pourcent < 5 and num > taille / 100 * 50 and num < taille / 100 * 60:
+                    print (" >> 50%")
+                    pourcent = 5
+                elif pourcent < 6 and num > taille / 100 * 60 and num < taille / 100 * 70:
+                    print (" >> 60%")
+                    pourcent = 6
+                elif pourcent < 7 and num > taille / 100 * 70 and num < taille / 100 * 80:
+                    print (" >> 70%")
+                    pourcent = 7
+                elif pourcent < 8 and num > taille / 100 * 80 and num < taille / 100 * 90:
+                    print (" >> 80%")
+                    pourcent = 8
+                elif pourcent < 9 and num > taille / 100 * 90 and num < taille / 100 * 100:
+                    print (" >> 90%")                    
+                    pourcent = 9
+                    
+                num = num + 1024
+
+
 while 1:
-
-
-
-    
+   
     commande = input("terminal>")
+    
     connexion_avec_client.send(commande.encode())
-    mess = b""
-    while mess == b"":
-        print (1)
-        mess = connexion_avec_client.recv(1024)
+    if commande.split()[0] == "upload":
+        d = receive_upload()
+        if d == 0:
+            pass        
+    else:
+        mess = b""
+        while mess == b"":
+            print (1)
+            mess = connexion_avec_client.recv(1024)
 
-    print(mess)
+        print(mess)
+
+
+
+
+
+
